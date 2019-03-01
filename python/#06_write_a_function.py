72a17eda25279bb2926c74e0a4e855755a525d15
Masahiro Onda <monda0524@gmail.com>
2019-03-01 21:17:41 +0900

    add #06

---
+++ python/#06_write_a_function.py
@@ -0,0 +1,20 @@
+'''
+Write a function
+'''
+
+def is_leap(year):
+    leap = False
+
+    if year % 4 == 0:
+        if year % 100 != 0:
+            leap = True
+        elif year % 400 == 0:
+            leap = True
+
+    return leap
+
+if __name__ == '__main__':
+    year = int(input())
+
+    print(is_leap(year))
+
