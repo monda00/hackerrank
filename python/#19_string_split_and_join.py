72333e0814fd239f77ccba8456675e437f173577
monda00 <monda0524@gmail.com>
2019-03-14 12:16:08 +0900

    add #19

---
+++ python/#19_string_split_and_join.py
@@ -0,0 +1,15 @@
+'''
+String Split and Join
+'''
+
+def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+
