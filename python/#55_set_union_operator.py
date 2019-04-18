c0791711a5e7468033b4096c53a0e9e91ed90df6
monda <monda0524@gmail.com>
2019-04-19 07:20:27 +0900

    add #55

---
+++ python/#55_set_union_operator.py
@@ -0,0 +1,12 @@
+'''
+Set.union() Operation
+'''
+
+if __name__ == '__main__':
+    e_num = int(input())
+    e_student = set(input().split())
+    f_num = int(input())
+    f_student = set(input().split())
+
+    print(len(e_student.union(f_student)))
+
