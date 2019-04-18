5cf1c560d29910135a3ce2a6a4abbd6ac013f6fc
monda00 <monda0524@gmail.com>
2019-04-18 09:25:27 +0900

    fix #54

--- python/#54_company_logo.py
+++ python/#54_company_logo.py
@@ -10,6 +10,6 @@ import sys
 from itertools import groupby
 
 if __name__ == '__main__':
-    print(*[c + " " + str(c_num) for c_num, c in sorted(len(list(v)), k)\
-            for k, v in groupby(sorted(input()))], key=lambda x: (x[1], -x[0]))
+    print(*[c + " " + str(c_num) for c_num, c in sorted([(len(list(v)), k)\
+            for k, v in groupby(sorted(input()))], key=lambda x: (-x[0], x[1]))][:3], sep='\n')
 
