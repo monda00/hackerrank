7991d3d2bdbd832175f3e5a75140b6938cf30bdb
monda <monda0524@gmail.com>
2019-04-25 08:11:26 +0900

    add #62

---
+++ python/#62_maximiza_it.py
@@ -0,0 +1,13 @@
+'''
+Maximize It!
+'''
+
+from itertools import product
+
+if __name__ == '__main__':
+    k, m = map(int, input().split())
+    n = (list(map(int, input().split()))[1:] for _ in range(k))
+    result = map(lambda x: sum(i**2 for i in x)%m, product(*n))
+
+    print(max(result))
+
