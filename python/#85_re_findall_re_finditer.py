4073d92c5d7b851403e667c7658f92de498287f3
monda00 <monda0524@gmail.com>
2019-05-15 07:53:16 +0900

    udpate #85

--- python/#85_re_findall_re_finditer.py
+++ python/#85_re_findall_re_finditer.py
@@ -7,5 +7,7 @@ import re
 if __name__ == '__main__':
     vowels = 'aeiou'
     consonants = 'qwrtypsdfghjklzxcvbnm'
-
+    mat = re.findall(r"(?<=[%s])([%s]{2,})[%s]" % (consonants, vowels, consonants),
+                     input(), flags=re.I)
+    print('\n'.join(mat or ['-1']))
 
