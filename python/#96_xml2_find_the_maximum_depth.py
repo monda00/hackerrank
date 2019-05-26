4002b3af0d36bd5803fc3d5ad813aa9a5c85241f
monda <monda0524@gmail.com>
2019-05-26 09:31:42 +0900

    add #96

---
+++ python/#96_xml2_find_the_maximum_depth.py
@@ -0,0 +1,24 @@
+'''
+XML2 - Find the Maximum Depth
+'''
+
+import xml.etree.ElementTree as etree
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    level += 1
+    if level > maxdepth:
+        maxdepth = level
+    for e in elem:
+        depth(e, level)
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
+
