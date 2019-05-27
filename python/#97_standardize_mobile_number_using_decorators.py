e010f7ab340afea5e1e43cbd21e0d9ca81953f29
monda00 <monda0524@gmail.com>
2019-05-27 12:38:11 +0900

    udpate #97

--- python/#97_standardize_mobile_number_using_decorators.py
+++ python/#97_standardize_mobile_number_using_decorators.py
@@ -4,7 +4,7 @@ Standardize Mobile Number Using Decorators
 
 def wrapper(f):
     def fun(l):
-        # complete the function
+        f(["+91 " + c[-10:-5] + " " + c[-5:] for c in l])
     return fun
 
 @wrapper
