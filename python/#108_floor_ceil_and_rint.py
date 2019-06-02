f40e0783764b1a6b9e3adf921baa22dc91090a8f
monda <monda0524@gmail.com>
2019-06-02 23:42:19 +0900

    update #108

--- python/#108_floor_ceil_and_rint.py
+++ python/#108_floor_ceil_and_rint.py
@@ -6,7 +6,7 @@ import numpy
 
 if __name__ == '__main__':
     inp_arr = numpy.array(input().split(), float)
-
+    numpy.set_printoptions(sign=' ')
     print(numpy.floor(inp_arr))
     print(numpy.ceil(inp_arr))
     print(numpy.rint(inp_arr))
